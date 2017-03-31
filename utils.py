64ef3749c84a40667e1accc3993012fe1e00c255
Siyao Zhang <zhangsiyao_1993@live.cn>
2017-03-31 12:01:35 +0800

    实现log

---
+++ utils.py
@@ -0,0 +1,37 @@
+import time
+import inspect
+
+
+def log(*args, **kwargs):
+    caller_info = caller()
+    fmt = '%Y-%m-%d %H:%M:%S'
+    value = time.localtime(int(time.time()))
+    dt = time.strftime(fmt, value)
+    if args is not ():
+        args = list(args)
+        args[0] = 'Log: ' + args[0]
+    with open('log.log', 'a', encoding='utf-8') as f:
+        print(dt, caller_info, *args, file=f, **kwargs)
+
+
+def caller():
+    frame_info = inspect.stack()
+    filename = frame_info[2][1]
+    lineno = frame_info[2][2]
+    caller_func = frame_info[2][3]
+    info = 'filename:{} lineno:{} function={}'.format(
+        filename, lineno, caller_func
+    )
+    return info
+
+
+def foo():
+    log('test in foo')
+
+
+if __name__ == '__main__':
+    foo()
+    log('test')
+    log('test log')
+    log('')
+    log()
